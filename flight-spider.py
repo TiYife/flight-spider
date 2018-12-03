234a5a3493423e4177ebe70f351a29629cad019e
Tian Yifei <13051087618@163.com>
2018-12-03 15:12:22 +0800

    可以获取到航班信息了

--- flight-spider.py
+++ flight-spider.py
@@ -3,27 +3,40 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-if __name__ == "__main__":
+
+def get_token():
     response = requests.get("https://zh.flightaware.com/live/map")
     response.enconding = "utf-8"
     text = response.text
     soup = BeautifulSoup(text, "html.parser")
-
-    print(soup.prettify)
+    # print(soup.prettify)
 
     pattern = re.compile(r"var mapGlobals = .*?", re.MULTILINE | re.DOTALL)
     script = soup.find("script", text=pattern)
-    print(script)
+    # print(script)
 
     if script:
         pattern = re.compile(r"\"VICINITY_TOKEN\":\"(((\d)|[a-z])*)\"", re.MULTILINE | re.DOTALL)
         match = pattern.search(script.text)
         if match:
-            print(match.group(1))
+            # print(match.group(1))
+            return match.group(1)
+    return None
+
+
+def get_airports():
+    response = requests.get("https://zh.flightaware.com/ajax/ignoreall/vicinity_airports.rvt")
+    response.enconding = "utf-8"
     #print(response.text)
-'''
-    print(response.cookies)
-VICINITY_TOKEN
-    print(response.content)
-    print(response.content.decode("utf-8"))
-'''
+
+
+def get_airlines():
+    token = get_token()
+    response = requests.get("https://zh.flightaware.com/ajax/ignoreall/vicinity_aircraft.rvt?&minLon=67.5&minLat=4.54833984375&maxLon=100.56884765625&maxLat=26.3671875&token="+token)
+    response.enconding = "utf-8"
+    print(response.text)
+
+if __name__ == "__main__":
+    #token = get_token()
+   # get_airports()
+    get_airlines()
